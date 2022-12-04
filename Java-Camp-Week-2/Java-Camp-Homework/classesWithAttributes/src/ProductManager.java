890ce87a8f8ad48419ae41a0ddf6dd109ce27643
Ycanga <canayunus@gmail.com>
2022-12-05 01:53:31 +0300

    YouTube Engin Demiroğ -Java-

---
+++ Java-Camp-Week-2/Java-Camp-Homework/classesWithAttributes/src/ProductManager.java
@@ -0,0 +1,10 @@
+public class ProductManager {
+    //JDBC
+    public void Add(Product product) {
+        System.out.println("Ürün eklendi: " + product.getName());
+    }
+
+    public void Add2(int id, String name, String description, int stockAmount, double price) {
+
+    }
+}
---
+++ Java-Camp-Week-2/Java-Camp-Homework/reCampDemo_Classes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
+        int sonuc = 0;
+        sonuc = dortIslem.topla(5,7);
+        System.out.println(sonuc);
+    }
+}
