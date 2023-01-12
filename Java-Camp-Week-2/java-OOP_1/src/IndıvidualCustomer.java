9ef9b67eb5c242ed8b98d8abd321c616bb42958a
Ycanga <canayunus@gmail.com>
2023-01-12 23:55:28 +0300

    YouTube Engin Demiroğ -Java-

---
+++ Java-Camp-Week-2/java-OOP_1/src/IndıvidualCustomer.java
@@ -0,0 +1,20 @@
+public class IndıvidualCustomer extends Customer{
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
