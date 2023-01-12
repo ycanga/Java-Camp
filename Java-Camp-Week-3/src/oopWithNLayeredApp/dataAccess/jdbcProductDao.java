cdd8f31c2d33df83ec525050c7ff21d232243774
Ycanga <canayunus@gmail.com>
2023-01-12 23:56:41 +0300

    YouTube Engin Demiroğ -Java-

---
+++ Java-Camp-Week-3/src/oopWithNLayeredApp/dataAccess/jdbcProductDao.java
@@ -0,0 +1,7 @@
+package oopWithNLayeredApp.dataAccess;
+import oopWithNLayeredApp.entities.Product;
+public class jdbcProductDao implements ProductDao{
+    public void add(Product product){
+        System.out.println("JDBC ile veritabanına eklendi.");
+    }
+}
